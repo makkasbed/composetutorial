bc8587bc9315579c67ef30afe86e318d7e5fc10e
adu <aluta182004@gmail.com>
2023-03-19 10:08:00 +0000

    created and added a composable function to android

--- app/src/main/java/com/logiclabent/composetutorial/MainActivity.kt
+++ app/src/main/java/com/logiclabent/composetutorial/MainActivity.kt
@@ -11,20 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.logiclabent.composetutorial.ui.theme.ComposeTutorialTheme
+import javax.xml.soap.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard("Android")
         }
     }
 }
@@ -32,6 +25,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+@Composable
+fun MessageCard(name: String){
+    Text(text="Hello $name")
 }
 
 @Preview(showBackground = true)
