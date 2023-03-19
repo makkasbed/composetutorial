939290cd228f971a0e8222feb6cc6640050cd545
adu <aluta182004@gmail.com>
2023-03-19 10:36:30 +0000

    implemented padding around layouts

--- app/src/main/java/com/logiclabent/composetutorial/MainActivity.kt
+++ app/src/main/java/com/logiclabent/composetutorial/MainActivity.kt
@@ -3,9 +3,6 @@ package com.logiclabent.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,6 +12,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.res.painterResource;
 import androidx.compose.foundation.Image;
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.logiclabent.composetutorial.ui.theme.ComposeTutorialTheme
 
 
@@ -32,13 +33,19 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message){
-    Row {
+    Row(modifier=Modifier.padding(all=8.dp)) {
         Image(
-            painter= painterResource(R.mipmap.ic_launcher),
-            contentDescription ="Contact Profile pic"
+            painter= painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription ="Contact Profile pic",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column {
             Text(text=msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
