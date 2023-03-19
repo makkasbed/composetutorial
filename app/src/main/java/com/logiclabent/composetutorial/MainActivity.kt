10c5c9ce5cf6f2c5e4a9efc76d907989188aa074
adu <aluta182004@gmail.com>
2023-03-19 11:19:14 +0000

    completed the compose tutorial

--- app/src/main/java/com/logiclabent/composetutorial/MainActivity.kt
+++ app/src/main/java/com/logiclabent/composetutorial/MainActivity.kt
@@ -4,22 +4,26 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.res.painterResource;
 import androidx.compose.foundation.Image;
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.*
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import com.logiclabent.composetutorial.data.SampleData
 import com.logiclabent.composetutorial.ui.theme.ComposeTutorialTheme
 
 
@@ -28,9 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(msg= Message(author = "Android", body = "Jetpack compose"))
-                }
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
@@ -49,6 +51,14 @@ fun Conversation(messages: List<Message>){
     }
 }
 
+@Preview
+@Composable
+fun PreviewConversation(){
+    ComposeTutorialTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
+
 @Composable
 fun MessageCard(msg: Message){
     Row(modifier=Modifier.padding(all=8.dp)) {
@@ -62,15 +72,28 @@ fun MessageCard(msg: Message){
         )
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded=!isExpanded }) {
             Text(text=msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(text = msg.body,
                     modifier= Modifier.padding(all = 4.dp),
+                    maxLines= if(isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2)
             }
 
